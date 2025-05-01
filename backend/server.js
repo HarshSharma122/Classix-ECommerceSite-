aff18a796317889148a43a207801fbb543dbd188
DsaBegin <harsh444577@gmail.com>
2025-05-01 17:39:43 +0530

    Project is uploaded
    Signed-off-by: DsaBegin <harsh444577@gmail.com>

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+
+dotenv.config(); // for getting env data
+
+const app = express();
+
+const port = process.env.PORT || 8000; // for getting port value from static env file
+/*Middlewares files here */
+app.use(express.json());
+app.use(express.static('/public'));
+app.use(cookieParser()); // for stting cookie on browser
+app.use(cors({
+    origin:[
+        process.env.ORIGIN
+    ],
+    methods:[
+        "GET", "POST", "PUT", "PATCH", "DELETE"
+    ],
+    credentials:true,
+}))
+
+
+
+
+
+
+
+
+
+
+app.listen(port, ()=>
+{
+    console.log(`Server is running at ${port} port`);
+})
